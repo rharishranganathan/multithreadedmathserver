5561637364006dc0b473f2e3800e92ba33f5c956
HARISH R <rharishranganathan@gmail.com>
2023-11-21 22:44:28 +0530

    Added socket code

--- math-server.py
+++ math-server.py
@@ -4,3 +4,8 @@ from threading import Thread
 
 HOST = ''
 PORT = 4444
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+s.bind((HOST, PORT))
+s.listen() # Look for calling bell
