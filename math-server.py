b51bcbb741a28cf8631ffcbb9cc46e1f495924c6
HARISH R <rharishranganathan@gmail.com>
2023-11-21 23:22:58 +0530

    Added function to init the server

--- math-server.py
+++ math-server.py
@@ -7,7 +7,15 @@ from comm_thread import ProcessOutputThread
 HOST = ''
 PORT = 4444
 
+def start_new_math_thread(conn, addr):
+	t = MathServerCommunicationThread(conn, addr)
+	t.start()
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 s.bind((HOST, PORT))
 s.listen() # Look for calling bell
+while True: # To accept many incoming connections. 
+	conn, addr = s.accept() # Open door 
+	start_new_math_thread(conn, addr)
+s.close()
